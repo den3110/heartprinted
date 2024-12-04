d0059346ccfeaf645c8a697dde93c342d162a0dc
den3110 <datistpham@gmail.com>
2024-12-05 02:19:45 +0700

    commit

--- backend/src/api/resources/order/order.controller.js
+++ backend/src/api/resources/order/order.controller.js
@@ -19,7 +19,9 @@ function generateRandomString(length) {
 
 // Hàm tạo PDF từ HTML
 const generatePDF = async (htmlContent) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   await page.setContent(htmlContent);
 
